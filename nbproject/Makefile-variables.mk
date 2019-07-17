7dbe25648911ffda52bbd7ae7a9c0e228a508ca2
wwweagle <wwweagle@gmail.com>
2019-07-17 18:15:37 +0800

    working copy:

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=f15313DAC.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/f15313DAC.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=f15313dac.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/f15313dac.x.tar
